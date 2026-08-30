1037fcd9ce794647eb9c03e8906f9531cb51c3de
あませ / paper-daemon <juriojuriojurio6@gmail.com>
2026-08-31 00:58:40 +0900

    Test VPN affiliate rendering safety

--- tests/test_affiliate.js
+++ tests/test_affiliate.js
@@ -8,20 +8,28 @@ const elements={
   result:{hidden:true,innerHTML:''}
 };
 
+global.location={href:'https://paper-daemon.github.io/vpn-needs-router/'};
 global.document={getElementById:id=>elements[id]};
 global.localStorage={getItem:()=>null,setItem(){}};
 global.window={AFFILIATE_OFFERS:[
   {enabled:false,name:'Disabled VPN',url:'https://disabled.example/',disclosure:'広告'},
-  {enabled:true,name:'Enabled VPN',url:'https://enabled.example/',disclosure:'広告'}
+  {enabled:true,name:'Enabled <VPN>',url:'https://enabled.example/?a=1&b=2',disclosure:'広告 <b>test</b>'},
+  {enabled:true,name:'Bad URL',url:'javascript:alert(1)',disclosure:'広告'}
 ]};
 
-require('../app.js');
+const {escHtml,safeOfferUrl}=require('../app.js');
 assert.equal(typeof elements.go.onclick,'function');
 elements.go.onclick();
 assert.equal(elements.result.hidden,false);
 assert.match(elements.result.innerHTML,/公衆Wi/);
 assert.doesNotMatch(elements.result.innerHTML,/Disabled VPN/);
-assert.match(elements.result.innerHTML,/Enabled VPN/);
+assert.match(elements.result.innerHTML,/Enabled &lt;VPN&gt;/);
+assert.doesNotMatch(elements.result.innerHTML,/Bad URL/);
+assert.doesNotMatch(elements.result.innerHTML,/javascript:/);
+assert.match(elements.result.innerHTML,/a=1&amp;b=2/);
+assert.match(elements.result.innerHTML,/広告 &lt;b&gt;test&lt;\/b&gt;/);
 assert.match(elements.result.innerHTML,/sponsored nofollow noopener/);
+assert.equal(safeOfferUrl('javascript:alert(1)'),'');
+assert.equal(escHtml('<script>'),'&lt;script&gt;');
 
-console.log('6 assertions PASS: disabled affiliate offers stay hidden');
+console.log('12 assertions PASS: affiliate offers are filtered and escaped');
